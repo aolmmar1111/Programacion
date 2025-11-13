d7428dd84e5bb5b409f8e69409cb633f0de530f2
aolmmar1111 <alejandro.delolmo1111@gmail.com>
2025-11-13 13:12:13 +0100

    a

--- arrays/src/main/java/alboran/dia13del11/EjercicioArraysFuncion.java
+++ arrays/src/main/java/alboran/dia13del11/EjercicioArraysFuncion.java
@@ -6,16 +6,18 @@ public class EjercicioArraysFuncion {
 
     private static final Random r = new Random();
 
-    /// 7.- Crear una funcion que devuelva un array de tamaño especificado en el parametro con numero aleatorios entre 1 y 30
-    /// 8.- Funcion que recibe un array de numero (Array del paso 7) y devuelve un nuevo array sin duplicados
-    /// 
+    /// 7.- Crear una funcion que devuelva un array de tamaño especificado en el
+    /// parametro con numero aleatorios entre 1 y 30
+    /// 8.- Funcion que recibe un array de numero (Array del paso 7) y devuelve un
+    /// nuevo array sin duplicados
+    ///
     /// main -> 7 y 8.
 
     public static void main(String[] args) {
 
-        int longitud = 100;
+        int longitud = 10;
 
-        int [] arrayInt = rellenarArray(longitud);
+        int[] arrayInt = rellenarArray(longitud);
         for (int i = 0; i < arrayInt.length; i++) {
             System.out.print(arrayInt[i] + " ");
         }
@@ -28,24 +30,25 @@ public class EjercicioArraysFuncion {
         }
     }
 
-    public static int[] rellenarArray (int longitud){
-        int [] arrayInt = new int[longitud]; 
-        
+    public static int[] rellenarArray(int longitud) {
+        int[] arrayInt = new int[longitud];
+
         for (int i = 0; i < arrayInt.length; i++) {
-            arrayInt[i] = r.nextInt(1,31);
+            arrayInt[i] = r.nextInt(1, 31);
         }
         return arrayInt;
     }
 
-    public static int[] quitarDuplicados(int [] arrayInt) {
+    public static int[] quitarDuplicados(int[] arrayInt) {
+
         for (int i = 0; i < arrayInt.length; i++) {
             for (int j = 0; j < i; j++) {
                 if (arrayInt[j] == arrayInt[i]) {
                     arrayInt[j] = 0;
                 }
-            }   
+            }
         }
+
         return arrayInt;
     }
-
 }
