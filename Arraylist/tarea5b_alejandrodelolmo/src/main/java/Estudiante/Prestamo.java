d3e837378298162b8eedb6601f197756e7834916
aolmmar1111 <alejandro.delolmo1111@gmail.com>
2026-01-20 11:09:06 +0100

    completada

--- Arraylist/tarea5b_alejandrodelolmo/src/main/java/Estudiante/Prestamo.java
+++ Arraylist/tarea5b_alejandrodelolmo/src/main/java/Estudiante/Prestamo.java
@@ -1,9 +1,36 @@
 package Estudiante;
 
+import libro.Libro;
+
+import java.time.LocalDate;
+
 public class Prestamo {
+    private LocalDate fechaPrestamo;
+    private int diasDevolver;
+    private LocalDate fechaDevolucion;
+    private Libro l1 = new Libro();
+    private Estudiante est1;
+    public Prestamo(LocalDate fechaPrestamo, Libro l1, Estudiante est1){
+        if (l1.equals(null)){
+            throw new IllegalArgumentException("Has introducido un libro que no existe");
+        }
+        if (est1.equals(null)){
+            throw new IllegalArgumentException("Has introducido un estudiante que no existe");
+        }
+        this.l1 = l1;
+        this.est1 = est1;
+        this.fechaPrestamo = fechaPrestamo;
+        this.fechaDevolucion = fechaPrestamo.plusDays(diasDevolver);
 
+    }
 
-    public static void main(String[] args) {
-        Estudiante est1 = new Estudiante("Alejandro","Del olmo","76857633L");
+    @Override
+    public String toString() {
+        return "Prestamo{" +
+                "fechaPrestamo=" + fechaPrestamo +
+                ", fechaDevolucion=" + fechaDevolucion +
+                ", l1=" + l1 +
+                ", est1=" + est1 +
+                '}';
     }
 }
