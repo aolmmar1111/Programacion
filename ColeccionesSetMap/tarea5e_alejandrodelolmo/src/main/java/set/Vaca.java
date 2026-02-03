1d4a90ed40f803007b6ee7c2a868e60592c853bd
aolmmar1111 <alejandro.delolmo1111@gmail.com>
2026-02-03 11:07:45 +0100

    tarea5e semi

---
+++ ColeccionesSetMap/tarea5e_alejandrodelolmo/src/main/java/set/Vaca.java
@@ -0,0 +1,69 @@
+package set;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Vaca {
+    private int id;
+    private LocalDate fechaNacimiento;
+    String descripcion;
+    public Vaca (int id, LocalDate fechaNacimiento,String descripcion){
+        this.id = id;
+        this.fechaNacimiento = fechaNacimiento;
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "Vaca{" +
+                "id='" + this.id + '\'' +
+                ", Fecha de nacimiento='" + this.fechaNacimiento + '\'' +
+                ", Descripcion='" + this.descripcion + '\'' +
+                '}';
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        // 1. Verificamos si es la misma instancia en memoria
+        if (this == o) return true;
+
+        // 2. Verificamos si el objeto es nulo o de una clase diferente
+        if (o == null || getClass() != o.getClass()) return false;
+
+        // 3. Comparamos el atributo identificador (dni)
+        Vaca Vaca = (Vaca) o;
+        return Objects.equals(id, Vaca.id);
+    }    
+    
+    @Override
+    public int hashCode() {
+        // Genera un código hash único basado en el contenido del ID
+        return Objects.hash(id);
+    }
+    
+}
---
+++ ColeccionesSetMap/tarea5e_alejandrodelolmo/src/main/java/set/Vaqueria.java
@@ -0,0 +1,14 @@
+package set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Vaqueria {
+    private Set<Vaca> listaVacas;
+
+    public Vaqueria(){
+        this.listaVacas = new HashSet<>();
+    }
+
+    
+}
